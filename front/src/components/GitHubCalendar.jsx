6b295adebbefb875c80181b8e405db0dffd60c6d
frontjsw <89008053+frontjsw@users.noreply.github.com>
2023-10-20 19:35:53 +0900

    패키지 추가, git calendar 추가

---
+++ front/src/components/GitHubCalendar.jsx
@@ -0,0 +1,34 @@
+import GitHubCalendar from 'react-github-calendar';
+import styled from 'styled-components';
+const Calendar = (props) => {
+    const selectLastHalfYear = (contributions) => {
+        const currentYear = new Date().getFullYear();
+        const currentMonth = new Date().getMonth();
+        const shownMonths = 4;
+
+        return contributions.filter((activity) => {
+            const date = new Date(activity.date);
+            const monthOfDay = date.getMonth();
+
+            return date.getFullYear() === currentYear && monthOfDay > currentMonth - shownMonths && monthOfDay <= currentMonth;
+        });
+    };
+
+    return (
+        <CalendarCustom
+            // 깃허브 계정
+            username={props.githubId}
+            // 사이즈 조정
+            blockSize={40}
+            transformData={selectLastHalfYear}
+            style={{}}
+            hideTotalCount={true}
+            hideMonthLabels={true}
+            hideColorLegend={true}
+        />
+    );
+};
+
+export default Calendar;
+
+const CalendarCustom = styled(GitHubCalendar)``;
---
+++ front/src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calendar from '../components/GitHubCalendar';
+
+const MainPage = () => {
+    return (
+        <>
+            <Calendar githubId={'frontjsw'} />
+        </>
+    );
+};
+
+export default MainPage;
